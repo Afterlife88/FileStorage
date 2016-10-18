743cf8e10d821f2e5fbf3d173d3d5a0032985879
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-18 19:12:14 +0300

    Added contracts

---
+++ src/FileStorage.Contracts/Services/IBlobService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FileStorage.Contracts.Services
+{
+    /// <summary>
+    /// Contract for the blob behaviour 
+    /// </summary>
+    public interface IBlobService
+    {
+        /// <summary>
+        /// Downloading file to the blob storage
+        /// </summary>
+        /// <param name="path">path to the file</param>
+        /// <returns></returns>
+        Task<Stream> DownloadFileAsync(string path);
+        /// <summary>
+        /// Uploading file to the blob storage
+        /// </summary>
+        /// <param name="httpContent">data</param>
+        /// <returns></returns>
+        Task<Stream> UploadFileAsync(HttpContent httpContent);
+        /// <summary>
+        /// Deleting file from the blob storage
+        /// </summary>
+        /// <param name="path">path to the file<</param>
+        /// <returns></returns>
+        Task DeleteFileAsync(string path);
+    }
+}
---
+++ src/FileStorage.Contracts/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileStorage.Contracts.Services
+{
+    /// <summary>
+    /// Managing file entity
+    /// </summary>
+    public interface IFileService
+    {
+    }
+}
