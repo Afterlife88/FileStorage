12d721abddf84bf847c96599503f6bae23120476
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-29 17:05:53 +0300

    Added filter on caller folder on replace controller

--- src/FileStorage.Web/wwwroot/app/controllers/modals-controllers/replace-controller.js
+++ src/FileStorage.Web/wwwroot/app/controllers/modals-controllers/replace-controller.js
@@ -5,9 +5,9 @@
       .module('app')
       .controller('replaceNodeController', replaceNodeController);
 
-  replaceNodeController.$inject = ['data', '$uibModalInstance', 'folderService', 'Alertify', '$scope', 'fileService'];
+  replaceNodeController.$inject = ['data', '$uibModalInstance', 'folderService', 'Alertify', '$scope', 'fileService', 'filterFilter'];
 
-  function replaceNodeController(data, $uibModalInstance, folderService, Alertify, $scope, fileService) {
+  function replaceNodeController(data, $uibModalInstance, folderService, Alertify, $scope, fileService, filterFilter) {
 
     var vm = this;
     vm.data = data;
@@ -17,15 +17,21 @@
     vm.ok = ok;
     activate();
 
-
     function activate() {
       return folderService.getListOfFolders().then(function (response) {
         vm.arrOfFolders = response;
-        console.log(vm.arrOfFolders);
+        if (vm.data.uniqueFolderId) {
+          var filteredArrayFromCallerFolder = vm.arrOfFolders.filter(function (el) {
+            return el.uniqueFolderId !== vm.data.uniqueFolderId;
+          });
+          vm.arrOfFolders = filteredArrayFromCallerFolder;
+        }
       }).catch(function (err) {
         Alertify.error(err.data);
       });
     }
+
+
     function ok(nodeData, directoryToMove) {
       if (nodeData.contentType) {
         console.log(nodeData);
@@ -40,7 +46,8 @@
           Alertify.error(err.data);
         });
       } else {
-
+        console.log(nodeData, directoryToMove);
+        console.log('folder')
       }
 
     }
