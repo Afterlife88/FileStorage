e775abaad61376024d9f0ed0bfc0fb6468e99511
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-30 22:20:02 +0200

    Scaleme

---
+++ src/FileStorage.Web/Controllers/DailyCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileStorage.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class DailyCheckController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        }
+}
