b3b6809b0164c4a33d97cdb5b4510627e498d0ec
Andrew Iasko <andrii.iasko@gmail.com>
2017-04-25 13:43:49 +0300

    Project updated to latest dotnet core

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM microsoft/dotnet:latest
+FROM microsoft/dotnet:1.0-sdk
 
 # Set env variables
 ENV ASPNETCORE_URLS http://*:5000
--- src/FileStorage.Web/Startup.cs
+++ src/FileStorage.Web/Startup.cs
@@ -31,10 +31,10 @@ namespace FileStorage.Web
             builder.AddJsonFile("appsettings.json");
             var connectionStringConfig = builder.Build();
 
-            services.AddDbContext<DataDbContext>(opt => opt.UseSqlServer(
-                connectionStringConfig.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<DataDbContext>(opt => opt.UseSqlServer(
+            //    connectionStringConfig.GetConnectionString("DefaultConnection")));
 
-            //services.AddDbContext<DataDbContext>(opt => opt.UseInMemoryDatabase());
+            services.AddDbContext<DataDbContext>(opt => opt.UseInMemoryDatabase());
 
             services.AddIdentity<ApplicationUser, IdentityRole>(pass =>
                  {
